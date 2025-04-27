8a37246848521aff86c901065556788ed411ee9a
nabin.dhami <nabin.dhami@swifttech.com.np>
2025-04-27 11:39:01 +0545

    feat: llimiter

--- src/server.js
+++ src/server.js
@@ -4,17 +4,21 @@ import { rateLimit } from "express-rate-limit";
 const app = express();
 const port = 3000;
 
-const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  limit: 2,
-  standardHeaders: true,
-  legacyHeaders: false,
+function limiter({ minute = 1, limit = 2 } = {}) {
+  return rateLimit({
+    windowMs: minute * 60 * 1000,
+    limit,
+    standardHeaders: true,
+    legacyHeaders: false,
+  });
+}
+
+app.get("/", limiter(), async (req, res) => {
+  // await new Promise((res) => setTimeout(res, 1000));
+  res.json({ message: "Hello World!" });
 });
-
-app.use(limiter);
-
-app.get("/", async (req, res) => {
-  await new Promise((res) => setTimeout(res, 5000));
+app.get("/hello", limiter({ limit: 3 }), async (req, res) => {
+  // await new Promise((res) => setTimeout(res, 1000));
   res.json({ message: "Hello World!" });
 });
 
